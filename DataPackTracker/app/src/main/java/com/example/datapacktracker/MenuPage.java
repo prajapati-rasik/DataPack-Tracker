da94aadbc962d7874c7ecab646714357501d48e1
Rasik Prajapati <56621182+arr-swartz@users.noreply.github.com>
2020-09-27 18:26:00 +0530

    Add files via upload

---
+++ DataPackTracker/app/src/main/java/com/example/datapacktracker/MenuPage.java
@@ -0,0 +1,31 @@
+package com.example.datapacktracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import home.HomeActivity;
+
+public class MenuPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_page);
+
+        Button signout = findViewById(R.id.sign_out);
+        signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(MenuPage.this, HomeActivity.class));
+                finish();
+            }
+        });
+    }
+}
